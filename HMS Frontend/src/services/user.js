2765a49a800101c6a66a750c2999eb18dfbe2828
emayurgadekar <engrmayurgadekar@gmail.com>
2024-02-10 18:47:37 +0530

    HMS Frontend Sign in and Register

---
+++ HMS Frontend/src/services/user.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import { createUrl, log } from '../utils/utils';
+
+export async function registerUser(
+  pname,
+  email,
+  password,
+  conatct,
+  age,
+  bloodGroup,
+  address,
+  role
+) {
+  const url = createUrl('/patient/register');
+  const body = {
+    pname,
+    email,
+    password,
+    conatct,
+    age,
+    bloodGroup,
+    address,
+    role
+  };
+
+  try {
+    // Check if the user already exists by email
+    // try {
+    //   // const getUserResponse = await axios.get(url);
+    //   // if (getUserResponse.data && getUserResponse.data.email === email) {
+    //   //   console.log(getUserResponse.data);
+    //   //   return { email: '' }; // Return an empty object to indicate existing email
+    //   // }
+    //   if(getUserResponse.data && getUserResponse.data.role === 'Patient'){
+    //     return {userRoles : 'Patient'}
+    //   }
+    //   else if (getUserResponse.data && getUserResponse.data.userRoles === 'ADMIN'){
+    //     return {userRoles : 'ADMIN'}
+    //   }
+    //   else if (getUserResponse.data && getUserResponse.data.userRoles === 'SALESPERSON'){
+    //     return {userRoles : 'SALESPERSON'}
+    //   }
+    // } catch (getUserEx) {
+    //   log('Error fetching user data by email:', getUserEx);
+    // }
+
+    // Proceed with registration if the email is not found in the database
+    const response = await axios.post(url, body);
+    log(response.data);
+    return response.data;
+  } catch (ex) {
+    log('Error registering user:', ex);
+    return null;
+  }
+}
+
+export async function loginUser(email, password) {
+  //const url = createUrl('/auth/signin')
+  const url = createUrl('/patient/signin')
+  const body = {
+    email,
+    password,
+  }
+
+  // wait till axios is making the api call and getting response from server
+  try {
+    const response = await axios.post(url, body)
+    log(response.data)
+    debugger; 
+    const token = response.data.jwt;
+    const userRoles = response.data.role;
+    const userId = response.data.id;
+    const isLoggedIn = true;//response.data.isLoggedIn;
+
+    sessionStorage.setItem("token" , token);
+    sessionStorage.setItem("userRoles", userRoles);
+    sessionStorage.setItem("userId", userId);
+    sessionStorage.setItem("isLoggedIn", isLoggedIn);
+    return response.data
+  } catch (ex) {
+    log(ex)
+    return null
+  }
+}
